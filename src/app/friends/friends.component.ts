110577d6a313e295f0bcb1e324e2741682517e78
bluohazard <greggygf@gmail.com>
2019-05-20 16:52:22 +0700

    add friend form

--- src/app/friends/friends.component.ts
+++ src/app/friends/friends.component.ts
@@ -7,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FriendsComponent implements OnInit {
 
+  friends = [
+    { name: 'Greggy', email: 'greggygf@gmail.com', contact: '@GreggyGF' },
+    { name: 'Gianini', email: 'greggygf@gmail.com', contact: '@GreggyGF' },
+    { name: 'Firmansyah', email: 'greggygf@gmail.com', contact: '@GreggyGF' },
+  ];
+
+  nama: string;
+  email: string;
+  contact: string;
+
+  addFriend() {
+    this.friends.push({ name: this.nama, email: this.email, contact: this.contact });
+  }
+
   constructor() { }
 
   ngOnInit() {
