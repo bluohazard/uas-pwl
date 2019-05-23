0278d30f7cc1fb413e9de95a132023033c93e1ab
bluohazard <greggygf@gmail.com>
2019-05-23 13:59:58 +0700

    set title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'UASFIX';
+  public constructor(private titleService: Title) { }
+
+  // tslint:disable-next-line:use-life-cycle-interface
+  ngOnInit() {
+    this.setTitle('UAS PWL - GreggyGF');
+  }
+
+  public setTitle(newTitle: string) {
+    this.titleService.setTitle(newTitle);
+  }
 }
